e9e2dc2eaf8278aa2f5059dbaa0360c4d108d9fc
Keely Muench <kmuench13@gmail.com>
2018-05-02 10:40:24 -0500

    add on click function

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -18,5 +18,25 @@ var config = {
   var totalBilled = 0;
 
 
-//   $("#add-employee").on("click", function(event) {
-//     event.preventDefault();
+  $("#add-employee").on("click", function(event) {
+    event.preventDefault();
+
+      name = $("#name").val().trim();
+      role = $("#role").val().trim();
+      startDate = $("#startDate").val().trim();
+    //   monthsWorked = $("#").val().trim();
+      monthlyRate = $("#monthlyRate").val().trim();
+    //   totalBilled = $("#").val().trim();
+
+    console.log(name);
+
+      database.ref().push({
+        name: name,
+        role: role,
+        startDate: startDate,
+        monthsWorked: monthsWorked,
+        monthlyRate: monthlyRate,
+        totalBilled: totalBilled
+      });
+
+  });
