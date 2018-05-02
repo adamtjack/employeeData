fea6488d3ea0608b7637ec931b0a5075ad9c1e59
Kevin Pena <kevinpena160@gmail.com>
2018-05-02 11:23:22 -0500

    combined click listeners

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -17,68 +17,52 @@ var monthsWorked = 0;
 var monthlyRate = 0;
 var totalBilled = 0;
 
+
 $("#add-employee").on("click", function (event) {
-
     event.preventDefault();
+
     var newRow = $("<tr>");
-
-    var name = $("#name").val();
-    var role = $("#role").val();
-    var startDate = $("#startDate").val();
-    var monthlyRate = $("#monthlyRate").val();
-
-    console.log(name);
-    console.log(role);
-    console.log(startDate);
-    console.log(monthlyRate);
+    var newColumn = $("<td>");
 
 
-});
+    name = $("#name").val().trim();
+    role = $("#role").val().trim();
+    startDate = $("#startDate").val().trim();
+    monthlyRate = $("#monthlyRate").val().trim();
 
-
-  $("#add-employee").on("click", function(event) {
-    event.preventDefault();
-
-      name = $("#name").val().trim();
-      role = $("#role").val().trim();
-      startDate = $("#startDate").val().trim();
-    //   monthsWorked = $("#").val().trim();
-      monthlyRate = $("#monthlyRate").val().trim();
-    //   totalBilled = $("#").val().trim();
-
-    console.log(name);
-
-      database.ref().push({
+    database.ref().push({
         name: name,
         role: role,
         startDate: startDate,
         monthsWorked: monthsWorked,
         monthlyRate: monthlyRate,
         totalBilled: totalBilled
-      });
+    });
 
-  });
+    $("#tableBody").append(newRow);
 
-          // Loop for making Month select form
-          for (i = 1; i < 13; i++) {
-            var newOption = $("<option>");
-            newOption.val(i);
-            newOption.text(i);
-            $("#startMonth").append(newOption);
-        }
-        // Loop for making Day select form
-        for (i = 1; i < 32; i++) {
-            var newOption = $("<option>");
-            newOption.val(i);
-            newOption.text(i);
-            $("#startDay").append(newOption);
-        }
-        // Loop for making Year select form
-        for (i = 1900; i < 2019; i++) {
-            var newOption = $("<option>");
-            newOption.val(i);
-            newOption.text(i);
-            $("#startYear").prepend(newOption);
-        }
-        $("#startYear").prepend("<option value='' selected disabled>Year</option>");
+});
+
+// Loop for making Month select form
+for (i = 1; i < 13; i++) {
+    var newOption = $("<option>");
+    newOption.val(i);
+    newOption.text(i);
+    $("#startMonth").append(newOption);
+}
+// Loop for making Day select form
+for (i = 1; i < 32; i++) {
+    var newOption = $("<option>");
+    newOption.val(i);
+    newOption.text(i);
+    $("#startDay").append(newOption);
+}
+// Loop for making Year select form
+for (i = 1900; i < 2019; i++) {
+    var newOption = $("<option>");
+    newOption.val(i);
+    newOption.text(i);
+    $("#startYear").prepend(newOption);
+}
+$("#startYear").prepend("<option value='' selected disabled>Year</option>");
 
