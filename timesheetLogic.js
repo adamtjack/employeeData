3ca3856e050665607f46d96803daa99d7844fa6b
Kevin Pena <kevinpena160@gmail.com>
2018-05-02 20:12:51 -0500

    working rows/columns

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -1,51 +1,88 @@
-var config = {
+
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyD3eFj9yLScyx6khBKsAnkrmRaVoDknJl4",
     authDomain: "employeedata-9485b.firebaseapp.com",
     databaseURL: "https://employeedata-9485b.firebaseio.com",
     projectId: "employeedata-9485b",
     storageBucket: "employeedata-9485b.appspot.com",
     messagingSenderId: "231854602629"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var name = "";
-var role = "";
-var startDate = "";
-var monthsWorked = 0;
-var monthlyRate = 0;
-var totalBilled = 0;
-var emplyeeCount = 0;
+$("#addButton").on("click", function (event) {
 
-
-$("#add-employee").on("click", function (event) {
     event.preventDefault();
 
+    // name = $("#employeeName").val();
+    // role = $("#employeeRole").val();
+    // startDate = $("#startDate").val();
+    // monthlyRate = $("#monthlyRate").val();
+
     var newRow = $("<tr>");
-    var newColumn = $("<td>");
+    var newColumn;
+    var employeeObj = {};
 
+    $("form").find("input").each( function (index, input){
+        var key = $(input).attr("name");
+        var val = $(input).val();
+        employeeObj[key] = val;
 
-    name = $("#name").val().trim();
-    role = $("#role").val().trim();
-    startDate = $("#startDate").val().trim();
-    monthlyRate = $("#monthlyRate").val().trim();
+        newColumn = $("<td>");
+        newColumn.text(val);
+        newRow.append(newColumn);
+    });
+
+    var monthsWorked = getMonthsWorked(employeeObj.startDate)
+    var monthsWorkedColumn = $("<td>");
+    monthsWorkedColumn.text(monthsWorked);
+    newRow.append(monthsWorkedColumn);
+
+    var totalBilled = monthsWorked * employeeObj.rate
+    var totalBilledColumn = $("<td>");
+    totalBilledColumn.text(totalBilled);
+    newRow.append(totalBilledColumn);
+
+    console.log(employeeObj.name)
+    console.log(employeeObj.role)
+    console.log(employeeObj.startDate)
+    console.log(monthsWorked)
+    console.log(totalBilled)
 
     database.ref().push({
-        name: name,
-        role: role,
-        startDate: startDate,
+        name: employeeObj.name,
+        role: employeeObj.role,
+        startDate: employeeObj.startDate,
         monthsWorked: monthsWorked,
-        monthlyRate: monthlyRate,
+        monthlyRate: employeeObj.rate,
         totalBilled: totalBilled
     });
 
-
-    newRow.attr("id", "employee-" + emplyeeCount);
     $("#tableBody").append(newRow);
-    newColumn.append(name)
-    $("#employee" + emplyeeCount).append(newColumn)
+
+    // newRow.attr("id", "employee-" + emplyeeCount);
+    // $("#tableBody").append(newRow);
+    // newColumn.append(name)
+
+    // $("#employee-" + emplyeeCount).append(newColumn);
+    // newColumn.append(role);
+
+    // $("#employee-" + emplyeeCount).append(newColumn);
+    // newColumn.append(startDate);
+
+    // $("#employee-" + emplyeeCount).append(newColumn);
+    // newColumn.append(monthlyRate);
+
+    // $("#employee-" + emplyeeCount).append(newColumn);
 
 
 });
 
+function getMonthsWorked (startDate) {
+    var today = moment();
+    startDate = moment(startDate);
+
+    return today.diff(startDate, "months")
+};
